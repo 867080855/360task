1eaaf884660f57ee09fa3700745696d9fb10e83c
867080855 <34697117+867080855@users.noreply.github.com>
2018-01-02 21:38:21 +0800

    Add files via upload

---
+++ 360/js/index.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+
+$(function(){
+    $('#dowebok').fullpage({
+    	sectionsColor:['#0da5d6', '#2AB561', '#DE8910', '#16BA9D', '#0DA5D6'],
+    	afterLoad:function(link,index){
+
+    		$('.section').removeClass('current');
+    		setTimeout(function(){
+    			$('.section').eq(index-1).addClass('current');
+
+    		},100)
+    	}
+    });
+});
